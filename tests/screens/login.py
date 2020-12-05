f5db3752b41c7eda44a05c67ef5f2aa394989f76
Aida Real <areal060781@gmail.com>
2020-12-05 15:32:24 -0500

    Add page objects

---
+++ tests/screens/login.py
@@ -0,0 +1,14 @@
+class LoginScreen:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_input = driver.find_element_by_id('username')
+        self.password_input = driver.find_element_by_id('password')
+        self.login_button = driver.find_element_by_id('login')
+
+    def login(self, username, password):
+        self.username_input.send_keys(username)
+        self.password_input.send_keys(password)
+        self.login_button.click()
+
+    def validate_button_disabled_when_bad_password(self):
+        assert self.login_button.is_enabled() == False
---
+++ tests/screens/main.py
@@ -0,0 +1,7 @@
+class MainScreen:
+    def __init__(self, driver):
+        self.driver = driver
+        self.welcome_message = driver.find_element_by_id('textview_first')
+
+    def validate_welcome_message(self, username):
+        assert self.welcome_message.text == 'Hello ' + username
