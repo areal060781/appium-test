0b7e1f0624595990f7a8ed57f370b5a411770d13
Aida Real <areal060781@gmail.com>
2020-12-05 15:29:44 -0500

    Define helpers for testing

---
+++ tests/helpers/appiumsetup.py
@@ -0,0 +1,13 @@
+EXECUTOR = 'http://127.0.0.1:4723/wd/hub'
+
+ANDROID_BASE_CAPS = {
+    'platformName': 'Android',
+    'platformVersion': '9.0',
+    'deviceName': 'Pixel_3a_API_28_x86',
+    'appPackage': 'io.grainchain.logintest',
+    'appActivity': '.ui.login.LoginActivity'
+}
+
+username = 'aida@test.com'
+password = 'Test*123'
+bad_password = "test"
